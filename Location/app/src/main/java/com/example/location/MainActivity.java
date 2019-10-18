991f535ca2938033070dd5a32a890c3cf9630ff0
therahul21 <50905177+therahul21@users.noreply.github.com>
2019-10-18 15:49:32 -0400

    1. Start app on phone boot up 2. Also added previous work for storing email, but here, in Java

--- Location/app/src/main/java/com/example/location/MainActivity.java
+++ Location/app/src/main/java/com/example/location/MainActivity.java
@@ -4,14 +4,18 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -43,15 +47,44 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtLocation;
     private TextView txtContinueLocation;
     private StringBuilder stringBuilder;
-
+    private EditText editText;
+    Button button;
+    String email;
 
     private boolean isContinue = false;
     private boolean isGPS = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        editText=(EditText)findViewById(R.id.editText);
+        button=(Button)findViewById(R.id.button);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String email1 = prefs.getString("email","");
+        editText.setText(email1);
+        email=email1;
+        Log.i("info", "onCreate: email1"+email1);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 email = editText.getText().toString();
+                 //to keep/save text in the text field
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("email",email);
+                editor.apply();
+                Log.i("info", "onClick: Butten is clicked " + email );
+            }
+        });
+
         this.txtContinueLocation =  findViewById(R.id.txtContinueLocation);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
@@ -194,7 +227,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     protected void takeAttendance_to_Server() {
-        String final_url = "https://hellotrioskitchener.appspot.com/take_attendance";
+        String final_url = "https://hellotrioskitchener.appspot.com/take_attendance/"+ email;
+        Log.i("info", "takeAttendance_to_Server: "+final_url);
 
         OkHttpClient okHttpClient = new OkHttpClient();
 
@@ -212,5 +246,12 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+
+
+
+
+
+
     }
 }
